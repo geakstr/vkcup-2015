417983d277272750c49aa05edb95204613d2d8f7
Dmitry Kharitonov <geakstr@me.com>
2015-03-16 21:14:18 +0300

    Solved problem C in practice

--- qualification_2/C/src/Main.java
+++ qualification_2/C/src/Main.java
@@ -2,112 +2,84 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	BufferedReader in;
-	PrintWriter out;
-	StringTokenizer st;
+  BufferedReader in;
+  PrintWriter out;
+  StringTokenizer st;
 
-	char[] s, t;
-	int n, m;
-	boolean was_left = false;
-	
-	int left_idx = 0, left_offset = 0;
+  char[] s, t;
+  int n, m;
 
-	boolean left(int l, int r) {
-		for (int i = l; i < r; i++) {
-			if (s[left_idx] == t[i]) {
-				left_idx++;
-				if (left_idx == n) {
-					return true;
-				}
-			}
-		}
-		left_offset = r;	
-		return false;
-	}
-	
-	boolean right(int l, int r) {
-		int idx = 0;
-		for (int i = l; i < r; i++) {
-			if (s[idx] == t[i]) {
-				idx++;
-				if (idx == n) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+  void solve() throws IOException {
+    s = in.readLine().toCharArray();
+    t = in.readLine().toCharArray();
 
-	boolean split(int pivot) {
-		if (!was_left) {
-			if (!left(left_offset, pivot)) {
-				return false;
-			} else {
-				was_left = true;
-			}
-		}
-		return right(pivot, m);
-	}
+    n = s.length;
+    m = t.length;
 
-	void solve() throws IOException {
-		s = in.readLine().toCharArray();
-		t = in.readLine().toCharArray();
+    int l = 0, r = m - 1;
+    boolean was_left = false, was_right = false;
 
-		n = s.length;
-		m = t.length;
-		
-		boolean[] set = new boolean[26];
-		for (int i = 0; i < n; i++) {
-			set[s[i] - 97] = true;
-		}
-		
-		int ans = 0;
-		for (int i = n; i <= m - n;) {
-			if (split(i)) {
-				ans++;
-				i++;
-			} else {
-				if (was_left) {
-					while (i <= m - n && !set[t[i] - 97]) {
-						i++;
-					}
-				} else {
-					i++;
-				}
-			}
-		}
+    for (int left_idx = 0; l < m; l++) {
+      if (s[left_idx] == t[l]) {
+        left_idx++;
+        if (left_idx == n) {
+          was_left = true;
+          break;
+        }
+      }
+    }
+    
+    if (!was_left) {
+      out.println(0);
+      return;
+    }
 
-		out.println(ans);
-	}
+    for (int right_idx = n - 1; r >= 0; r--) {
+      if (s[right_idx] == t[r]) {
+        right_idx--;
+        if (right_idx == -1) {
+          was_right = true;
+          break;
+        }
+      }
+    }
+    
+    if (!was_right || r < l) {
+      out.println(0);
+      return;
+    }
+    
+    out.println(r - l);
+  }
 
-	Main() throws IOException {
-		in = new BufferedReader(new InputStreamReader(System.in));
-		out = new PrintWriter(System.out);
-		solve();
-		in.close();
-		out.close();
-	}
+  Main() throws IOException {
+    in = new BufferedReader(new InputStreamReader(System.in));
+    out = new PrintWriter(System.out);
+    solve();
+    in.close();
+    out.close();
+  }
 
-	String ns() throws IOException {
-		while (st == null || !st.hasMoreTokens()) {
-			st = new StringTokenizer(in.readLine());
-		}
-		return st.nextToken();
-	}
+  String ns() throws IOException {
+    while (st == null || !st.hasMoreTokens()) {
+      st = new StringTokenizer(in.readLine());
+    }
+    return st.nextToken();
+  }
 
-	int ni() throws IOException {
-		return Integer.parseInt(ns());
-	}
+  int ni() throws IOException {
+    return Integer.parseInt(ns());
+  }
 
-	double nd() throws IOException {
-		return Double.parseDouble(ns());
-	}
+  double nd() throws IOException {
+    return Double.parseDouble(ns());
+  }
 
-	long nl() throws IOException {
-		return Long.parseLong(ns());
-	}
+  long nl() throws IOException {
+    return Long.parseLong(ns());
+  }
 
-	public static void main(String[] args) throws IOException {
-		new Main();
-	}
+  public static void main(String[] args) throws IOException {
+    new Main();
+  }
 }
